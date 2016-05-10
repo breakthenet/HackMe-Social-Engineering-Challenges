a341c31e98f90497fcbcea30a768b6d91ea6e941
emeth- <seanybob@gmail.com>
2016-05-09 21:04:19 -0500

    updates

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('',
     url(r'^catch_email$', "api.views.catch_email"),
     url(r'^catch_sms$', "api.views.catch_sms"),
     url(r'^catch_phone_call$', "api.views.catch_phone_call"),
+    url(r'^get_config$', "api.views.get_config"),
     url(r'^$', "api.views.load_frontend")
 )
--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 import json
 from django.http import HttpResponseRedirect
 import os
+import requests
 
 def catch_email(request):
     """
@@ -9,19 +10,40 @@ def catch_email(request):
         forward("http://hm-spoof.herokuapp.com/catch_email")	
     """
     
-    mailgun_domain = os.environ.get('MAILGUN_DOMAIN', '')
+    print "request.POST['to'].split('@')[0]", request.POST['to'].split("@")[0]
+    print "request.POST['sender'][0]", request.POST['sender'][0]
     
-    print "request.POST", str(request.POST)
-    print "request.GET", str(request.GET)
-    
-    print "request.POST['recipient']", request.POST['recipient']
-    to_address = request.POST['recipient']
-    to_prefix = hash_email.split("@")[0]  # hash@email.com
-    print "request.POST['stripped-text']", request.POST['stripped-text']
+    if request.POST['to'].split("@")[0] == "bobdole":
+        if request.POST['sender'][0] == "joe@"+os.environ.get('MAILGUN_DOMAIN', ''):
+            plaintext = 'Hey! You may not have lost the password, as I just recently updated it so that could be why you are having trouble logging in. The new password is "eggroll". -Bob Dole'
+        
+            email_data = {
+                "from": "Bob Dole <bobdole@"+os.environ.get('MAILGUN_DOMAIN', '')+">",
+                "to": [request.POST['from']],
+                "subject": request.POST['subject'],
+                "html": plaintext,
+                "text": plaintext
+            }
+            print "email_data", email_data
+            #response = requests.post(
+            #    "https://api.mailgun.net/v3/{domain}/messages".format(domain=os.environ.get('MAILGUN_DOMAIN', '')),
+            #    auth=("api", os.environ.get('MAILGUN_API_KEY', '')),
+            #    data=email_data,
+            #    verify=False
+            #)
+            #print "[MAILGUN EMAIL API CALL=", response.status_code, response.text
+            #if response.status_code == 400:
+            #    print response.json()['message']
     
     return HttpResponse(json.dumps({
         "status": "success"
     }), content_type='application/json', status=200)
+
+def get_config(request):
+    return HttpResponse(json.dumps({
+        "challenge_1_email": "bobdole@"+os.environ.get('MAILGUN_DOMAIN', '')
+    }), content_type='application/json', status=200)
+
 
     
 def load_frontend(request):
